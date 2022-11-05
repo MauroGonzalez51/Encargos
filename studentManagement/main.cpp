7aaed3873fa8ae8355c56e7bce47afabff788025
MauroGonzalez <mauroalonso.g.f2004@gmail.com>
2022-11-04 19:04:27 -0500

    Nuevo Encargo en progreso

---
+++ studentManagement/main.cpp
@@ -0,0 +1,160 @@
+#include <bits/stdc++.h>
+#include <conio.h>
+
+const std::string folderPath = "studentManagement/files/";
+
+const int cantidadEstudiantes = 0;
+
+struct {
+    std::string nombre;
+    std::string clave;
+    std::string genero;
+    int asignatura;
+    double notaFinal;
+} estudiantes[cantidadEstudiantes];
+
+// ------------------------------------------------------------------------------------------------|
+
+std::ofstream logFile (folderPath +  "logFile.txt", std::ios::app);
+
+void escribirLinea(const int cantidadCaracteres, const std::string c) {
+    logFile << std::endl;
+    for (int i = 0; i < cantidadCaracteres; i++) 
+        logFile << c;
+    logFile << std::endl;
+}
+
+const char* horaActual() {
+    time_t now = time(0);
+    const char* horaActual = ctime(&now);
+
+    return horaActual;
+}
+
+void escribirLog() {
+    if (logFile.is_open()) {
+        escribirLinea(80, "-");
+        logFile << "Programa iniciado: " << horaActual() << std::endl;
+    }
+}
+
+void escribirLog(const std::string mensaje) {
+    if (logFile.is_open())
+        logFile << "[INFO] " << mensaje << std::endl;
+
+}
+
+// ------------------------------------------------------------------------------------------------|
+
+bool crearUsuario(const std::string usuario, const std::string clave) {
+    bool archivoCreado = false;
+
+    // if ((archivo.is_open()) && (!std::filesystem::exists(folderPath +  usuario + ".txt"))) {
+    //     archivo << usuario << std::endl;
+    //     archivo << clave << std::endl;
+    //     archivoCreado = true;
+    // } else 
+    //     escribirLog("El usuario: " + usuario + " Ya existe -> no se creara un nuevo archivo");
+    // std::cout << std::boolalpha << std::filesystem::exists(folderPath + usuario + ".txt") << std::endl;
+
+    if (std::filesystem::exists(folderPath + usuario + ".txt"))
+        escribirLog("El usuario: " + usuario +  " ya existe -> no se creara un nuevo archivo");
+    else {
+        std::ofstream archivo (folderPath + usuario + ".txt", std::ios::app);
+        if (archivo.is_open()) {
+            archivo << usuario << std::endl;
+            archivo << clave << std::endl;
+            archivoCreado = true;
+        }
+        archivo.close();
+    } 
+
+
+    return archivoCreado;
+}
+
+void inicializarUsuarios() {
+    std::vector <std::string> usuarios = {"luis", "jorge", "james", "johan", "kevin"};
+    std::vector <std::string> claves = {"123", "456", "789", "abc", "efg"};
+
+    for (int i = 0; i < 5; i++) {
+        if (crearUsuario(usuarios.at(i), claves.at(i)))
+            escribirLog("Usuario Creado: " +  usuarios.at(i));
+    }
+}
+
+// ------------------------------------------------------------------------------------------------|
+
+void escribirLinea(const std::string c) {
+    std::cout << std::endl;
+    for (int i = 0; i < 40; i++) 
+        std::cout << c;
+    std::cout << std::endl;
+} 
+
+// ---------------------------------------------------------------------|
+
+bool validarNombre(const std::string usuario) {
+    std::ifstream archivo (folderPath + usuario + ".txt");
+    bool esValido = false;
+
+    if (archivo.is_open()) {
+        std::string auxUsuario;
+
+        std::getline(archivo, auxUsuario);
+        if (usuario == auxUsuario) esValido = true;
+    }
+
+    return esValido;
+}
+
+// ---------------------------------------------------------------------|
+
+bool validarGenero(const std::string c) {
+    bool esValido = false;
+    if ((c == "m") || (c == "f")) esValido = true;
+    return esValido;
+}
+
+// ---------------------------------------------------------------------|
+
+void pedirDatos() {
+    // std::cout << "Login de Usuario" << std::endl << std::endl;
+    for (int i = 0; i < cantidadEstudiantes; i++) {
+        escribirLinea("*");
+        int cantidadIntentos = 3;
+
+        do {
+            std::cout << "Estudiante [" << i + 1 << "]" << std::endl;
+
+            do {
+                std::cout << "Ingrese su nombre: ";
+                std::getline(std::cin, estudiantes[i].nombre, '\n');
+
+                if (validarNombre(estudiantes[i].nombre)) break;
+                else cantidadIntentos--;
+            } while ((true) && (cantidadIntentos > 0));
+
+            do {
+                std::cout << "Ingrese su genero (m/f): ";
+                std::cin >> estudiantes[i].genero;
+
+                if (validarGenero(estudiantes[i].genero)) break;
+                else cantidadIntentos--;
+            } while ((true) && (cantidadIntentos > 0));
+
+            break;
+
+        } while (cantidadIntentos > 0);
+    }
+
+}
+
+
+// ------------------------------------------------------------------------------------------------|
+int main(void) {
+    escribirLog();
+    inicializarUsuarios();
+    pedirDatos();
+    return EXIT_SUCCESS;
+}
