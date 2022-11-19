2ca04441f054bc66caedf6d967417e0cfb408553
MauroGonzalez <mauroalonso.g.f2004@gmail.com>
2022-11-18 21:02:02 -0500

    Adding comments to it

--- ReadingCSV/dynamicSorting.cpp
+++ ReadingCSV/dynamicSorting.cpp
@@ -9,19 +9,26 @@ std::vector <float> breakpoints = {1, 1.5, 2, 2.5, 3, 3.5, 4.5, 5};
 // * of the categories are created later
 
 
+// !Important:
+    // ? To access all the files, remmember that all of them are named as the breakpoints
+
 void categorizePeople(std::vector <float> dataVector) {
     const std::string categoriesFolderPath = folderPath + "categoryFiles/";
+
+    // ! Loop to iterates from the end to the beginning of the array given
     for (auto valueManager = dataVector.rbegin(); valueManager != dataVector.rend(); valueManager++) {
         
+        // ! Check with the value given if it fits in any category -> Looping again
         for (int breakpointManager = 0; breakpointManager < breakpoints.size(); breakpointManager++) {
             
+            // ! For every iteration it opens a file name as the breakpoint is checking at the moment
             std::ofstream categoryFile (categoriesFolderPath + std::to_string(breakpoints.at(breakpointManager)) + ".txt", std::ios::app);
 
+            // ! The value is a pointer so for accesing the value, it's necesary the "*"
             if (*valueManager <= breakpoints.at(breakpointManager)) {
-
-
+                
+                // * Write it into the file -> with the "," for upcoming values 
                 categoryFile << *valueManager << ",";
-
                 break;
             }
 
