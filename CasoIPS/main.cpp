f5b301bc41c9a48126303a9b69e0bc9a52574f99
MauroGonzalez <mauroalonso.g.f2004@gmail.com>
2022-11-23 11:52:16 -0500

    Terminado la llenada de datos

---
+++ CasoIPS/main.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+struct Paciente {
+    std::string numeroIdentificacion, nombrePaciente, codigoEmpresa, fechaExamen = "";
+    int edad, intGenero, tipoExamen;
+    float estatura, peso;
+    char charGenero;
+};
+
+#include "llenarDatos.h"
+#include "registroDatos.h"
+
+
+void escribirLinea(const int n, const std::string c) {
+    std::cout << std::endl;
+    for (int i = 0; i < n; i++)
+        std::cout << c;
+    std::cout << std::endl;
+}
+
+bool validarMensaje(char msg[3]) {
+    // Basicamente lo que hace es validar si la opcion ingresada coincide con el "total" de opciones posibles
+    std::array <std::string, 20> options = {"Si", "No", "si", "no", "SI", "NO", "S", "N", "s", "n", "y", "n", "Y", "N", "YES", "NO", "yes", "no", "Yes", "No"};
+    // Flag = false significa que el programa va a salir
+    bool Flag = false;
+    for (int i = 0; i < options.size(); i++) {
+        if (msg == options[i]) {
+            Flag = true;
+            if (i % 2 != 0){
+                Flag = false;
+            }
+        }
+    }
+    return Flag;
+}
+
+void clearBuffers() { fflush(stdin); }
+
+void mainMenu() {
+    escribirLinea(50, "*");
+    std::cout << "Sistema MEGASALUD IPS" << std::endl;
+    std::cout << std::endl << std::endl;
+    do {
+        Paciente paciente;
+        llenarStruct(paciente);
+        char msg[3];
+        std::cout << "Desea agregar otro paciente?" << std::endl;
+        std::cout << "-> ";
+        std::cin >> msg;
+
+        if (!validarMensaje(msg))
+            break;
+
+    } while (true);
+} 
+
+int main(void) {
+    mainMenu();
+
+
+    return EXIT_SUCCESS;
+}
