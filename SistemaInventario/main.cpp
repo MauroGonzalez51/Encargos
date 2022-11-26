c1a5e6223e3fb57c553fd6c78080c38a161e01d9
MauroGonzalez <mauroalonso.g.f2004@gmail.com>
2022-11-25 19:21:18 -0500

    Usando "usign namespace std" a peticion

--- SistemaInventario/main.cpp
+++ SistemaInventario/main.cpp
@@ -1,72 +1,65 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+
 struct {
-    std::vector <std::string> nombreProductos;
-    std::vector <float> precioUnitario;
+    vector <string> nombreProductos;
+    vector <float> precioUnitario;
 } Inventario;
 
-void escribirLinea(const std::string c) {
-    std::cout << std::endl;
+void escribirLinea(const string c) {
+    cout << std::endl;
     for (int i = 0; i < 40; i++)
-        std::cout << c;
-    std::cout << std::endl;
+        cout << c;
+    cout << std::endl;
 }
 
 void verInventario() {
     if (Inventario.nombreProductos.size() > 0) {
         for (int i = 0; i < Inventario.nombreProductos.size(); i++) {
             escribirLinea("*");
-            std::cout << "Identificador: " << i + 1 << std::endl;
-            std::cout << "Nombre del Producto: " << Inventario.nombreProductos.at(i) << std::endl;
-            std::cout << "Precio Unitario: " << Inventario.precioUnitario.at(i) << std::endl;
+            cout << "Identificador: " << i + 1 << std::endl;
+            cout << "Nombre del Producto: " << Inventario.nombreProductos.at(i) << std::endl;
+            cout << "Precio Unitario: " << Inventario.precioUnitario.at(i) << std::endl;
         }
     } else 
-        std::cout << std::endl << "No hay productos registrados en el inventario" << std::endl;
+        cout << std::endl << "No hay productos registrados en el inventario" << std::endl;
 }
 
-bool validarMensaje(char msg[3]) {
-    // Basicamente lo que hace es validar si la opcion ingresada coincide con el "total" de opciones posibles
-    std::array <std::string, 20> options = {"Si", "No", "si", "no", "SI", "NO", "S", "N", "s", "n", "y", "n", "Y", "N", "YES", "NO", "yes", "no", "Yes", "No"};
-    // Flag = false significa que el programa va a salir
-    bool Flag = false;
-    for (int i = 0; i < options.size(); i++) {
-        if (msg == options[i]) {
-            Flag = true;
-            if (i % 2 != 0){
-                Flag = false;
-            }
-        }
-    }
-    return Flag;
+bool validarMensaje(const string mensajeIngresado) {
+    bool esValido = false;
+    vector <std::string> opcionesValidasTrue = {"Si", "si", "SI", "S", "s", "y", "Y", "YES", "yes", "Yes"};
+    for (auto &opcion : opcionesValidasTrue) { if (mensajeIngresado == opcion) esValido = true; }
+    return esValido;
 }
 
 void agregarProducto() {
     do {
-        std::string nombreProducto;
+        string nombreProducto;
         float precioUnitario;
 
         escribirLinea("/");
-        std::cout << "Ingrese el nombre del producto: ";
-        std::cin >> nombreProducto;
+        cout << "Ingrese el nombre del producto: ";
+        cin >> nombreProducto;
 
-        std::cout << "Ingrese el precio Unitario del producto: ";
-        std::cin >> precioUnitario;
+        cout << "Ingrese el precio Unitario del producto: ";
+        cin >> precioUnitario;
 
         Inventario.nombreProductos.push_back(nombreProducto);
         Inventario.precioUnitario.push_back(precioUnitario);
 
-        char msg[3];
-        std::cout << "Desea agregar otro producto?" << std::endl;
-        std::cout << "-> ";
-        std::cin >> msg;
+        string mensajeSalida;
+        cout << "Desea agregar otro producto?" << std::endl;
+        cout << "-> ";
+        cin >> mensajeSalida;
 
-        if (!validarMensaje(msg))
+        if (!validarMensaje(mensajeSalida))
             break;
 
     } while (true);
 }
 
-bool crearOrden(std::string nombreCliente, int cantidadOrdenada, int identificadorProducto) {
+bool crearOrden(string nombreCliente, int cantidadOrdenada, int identificadorProducto) {
     bool success = false;
     std::ofstream archivo ("SistemaInventario/pedidos/Recibo-" + nombreCliente + ".txt", std::ios::app);
 
@@ -79,7 +72,7 @@ bool crearOrden(std::string nombreCliente, int cantidadOrdenada, int identificad
             success = true;
         } catch (...) {
             archivo << "NULL" << std::endl;
-            std::cout << "Producto no encontrado" << std::endl;
+            cout << "Producto no encontrado" << std::endl;
         }
     }
     return success;
@@ -88,32 +81,32 @@ bool crearOrden(std::string nombreCliente, int cantidadOrdenada, int identificad
 void hacerPedido() {
     verInventario();
     escribirLinea("/");
-    std::string nombreCliente;
+    string nombreCliente;
     int cantidadOrdenada, identificadorProducto;
 
-    std::cout << "Ingrese su nombre: ";
-    std::cin >> nombreCliente;
+    cout << "Ingrese su nombre: ";
+    cin >> nombreCliente;
 
-    std::cout << "Ingrese el identificador del producto: ";
-    std::cin >> identificadorProducto;
+    cout << "Ingrese el identificador del producto: ";
+    cin >> identificadorProducto;
 
-    std::cout << "Ingrese la cantidad deseada: ";
-    std::cin >> cantidadOrdenada;
+    cout << "Ingrese la cantidad deseada: ";
+    cin >> cantidadOrdenada;
 
     (crearOrden(nombreCliente, cantidadOrdenada, identificadorProducto)) ? 
-        std::cout << std::endl << "Orden realizada Correctamente" << std::endl :
-            std::cout << std::endl << "Error al momento de realizar la orden" << std::endl;
+        cout << std::endl << "Orden realizada Correctamente" << std::endl :
+            cout << std::endl << "Error al momento de realizar la orden" << std::endl;
 }
 
 int mainMenuDashboard() {
     escribirLinea("-");
     int opcion;
-    std::cout << "1. Ver Inventario" << std::endl;
-    std::cout << "2. Agregar a Inventario" << std::endl;
-    std::cout << "3. Hacer un Pedido" << std::endl;
-    std::cout << "4. Salir" << std::endl;
-    std::cout << "-> ";
-    std::cin >> opcion;
+    cout << "1. Ver Inventario" << std::endl;
+    cout << "2. Agregar a Inventario" << std::endl;
+    cout << "3. Hacer un Pedido" << std::endl;
+    cout << "4. Salir" << std::endl;
+    cout << "-> ";
+    cin >> opcion;
 
     return opcion;
 }
